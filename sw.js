9328fc3d8d3c08782aae64a2ac3e6a5e246eddec
hlesbrown <info@hlesbrown.com>
2026-02-12 06:49:58 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "les-dashboard-v1";
+const CACHE_NAME = "les-dashboard-v2";
 const ASSETS = ["index.html", "manifest.json", "icon-192.png", "icon-512.png"];
 
 self.addEventListener("install", (e) => {
